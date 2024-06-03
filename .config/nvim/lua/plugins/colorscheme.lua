ae31ed1ba2b1e92b76f52df8e5cf578433741c64
Christian Grothe <christian.grothe@posteo.de>
2024-06-03 07:26:35 +0200

    new stuff

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,8 +2,8 @@ return {
 	{
 		"rebelot/kanagawa.nvim",
 		config = function()
-			vim.cmd.colorscheme("kanagawa-wave")
 			-- vim.cmd.colorscheme("kanagawa")
+			vim.cmd.colorscheme("kanagawa-wave")
 			-- vim.cmd.colorscheme("kanagawa-dragon")
 			--vim.cmd.colorscheme("kanagawa-lotus")
 		end,
