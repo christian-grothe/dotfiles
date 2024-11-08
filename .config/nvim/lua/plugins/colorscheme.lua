2015541a0969953f8e319955a7812022fd1e6c82
Christian Grothe <christian.grothe@posteo.de>
2024-11-08 20:06:14 +0100

    sway

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     "rebelot/kanagawa.nvim",
     config = function()
       -- vim.cmd.colorscheme("kanagawa")
-      vim.cmd.colorscheme("kanagawa-wave")
+      -- vim.cmd.colorscheme("kanagawa-wave")
       -- vim.cmd.colorscheme("kanagawa-dragon")
       -- vim.cmd.colorscheme("kanagawa-lotus")
     end,
@@ -14,7 +14,24 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-      -- vim.cmd.colorscheme("kanagawa-paper")
+      vim.cmd.colorscheme("kanagawa-paper")
     end,
-  }
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      --      vim.cmd.colorscheme("tokionight-night")
+    end,
+  },
+  {
+    "vague2k/vague.nvim",
+    config = function()
+      require("vague").setup({
+        -- vim.cmd.colorscheme("vague")
+      })
+    end
+  },
 }
